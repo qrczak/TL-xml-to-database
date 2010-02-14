925243ed5b2bda4e46a89117b6a46f61aed64f07
Kamil Kuzminski <kamil.kuzminski@gmail.com>
2010-02-14 16:10:31 +0100

    Added cron jobs
    Added cron jobs and removed redundant comments.

--- config/config.php
+++ config/config.php
@@ -25,54 +25,10 @@
  * @license    GNU/LGPL 
  * @filesource
  */
-
-
+ 
 /**
- * -------------------------------------------------------------------------
- * BACK END MODULES
- * -------------------------------------------------------------------------
- *
- * Back end modules are stored in a global array called "BE_MOD". Each module 
- * has certain properties like an icon, an optional callback function and one 
- * or more tables. Each module belongs to a particular group.
- * 
- *   $GLOBALS['BE_MOD'] = array
- *   (
- *       'group_1' => array
- *       (
- *           'module_1' => array
- *           (
- *               'tables'       => array('table_1', 'table_2'),
- *               'key'          => array('Class', 'method'),
- *               'callback'     => 'ClassName',
- *               'icon'         => 'path/to/icon.gif',
- *               'stylesheet'   => 'path/to/stylesheet.css',
- *               'javascript'   => 'path/to/javascript.js'
- *           )
- *       )
- *   );
- * 
- * Use function array_insert() to modify an existing modules array.
+ * Cron jobs
  */
-
-
-/**
- * -------------------------------------------------------------------------
- * FRONT END MODULES
- * -------------------------------------------------------------------------
- *
- * List all fontend modules and their class names.
- * 
- *   $GLOBALS['FE_MOD'] = array
- *   (
- *       'group_1' => array
- *       (
- *           'module_1' => 'Contentlass',
- *           'module_2' => 'Contentlass'
- *       )
- *   );
- * 
- * Use function array_insert() to modify an existing CTE array.
- */
+$GLOBALS['TL_CRON']['daily'][] = array('XmlToDb', 'fetchXmlFile');
  
 ?>
