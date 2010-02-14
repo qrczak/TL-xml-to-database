f56386494e60d9ab19676514c94647607a0d32e7
Qrczak <qrczak@gmail.com>
2010-02-14 19:30:02 +0100

    update sql file

--- config/database.sql
+++ config/database.sql
@@ -16,9 +16,7 @@
 
 CREATE TABLE `tl_xmltodb` (
   `id` int(10) unsigned NOT NULL auto_increment,
-  `pid` int(10) unsigned NOT NULL default '0',
   `sorting` int(10) unsigned NOT NULL default '0',
   `tstamp` int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (`id`),
-  KEY `pid` (`pid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
